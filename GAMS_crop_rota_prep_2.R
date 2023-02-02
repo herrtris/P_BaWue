ebbf257fada0b23d18a6fb6fd7d4653467cf18cb
Tristan Herrmann <tristan.herrmann@uni-hohenheim.de>
2023-02-02 12:24:43 +0100

    kommune CR_id anpassen rotation_matric_full fix

--- GAMS_crop_rota_prep_2.R
+++ GAMS_crop_rota_prep_2.R
@@ -113,29 +113,21 @@ write_xlsx(x=kommune, path = "C:/Users/User/OneDrive - bwedu/Dokumente/Landwirts
 #                                                     END - files creation from own QGIS work                                       ####
 
 ############################################################################################################################################
-## Intermediate step harmonizing both files
-## approach: take only the LAU_IDs that are within the kommune set
+# READING IN CROP ROTATIONS, developed out of crop rota.gms stored under C: Gams und dem Rscript CROPROTGDX_2.R 
+
 kommune %>% count()
-Rotations_felix %>% distinct(LAU_ID) %>% count()
 
-test1 <- kommune %>% mutate(LAU_ID=AGS_0_2) %>% right_join(Rotations_felix, by="LAU_ID")
-test1 %>% distinct(LAU_ID) %>% count()
-
-
-
-####################################################################################################
-
-
-# READING IN CROP ROTATIONS FROM FELIX's FILE CROP ROTATION - LATER NEEDS TO BE OWN FILE
-Rotations_felix <- read.csv("C:\\Users\\User\\OneDrive - bwedu\\Dokumente\\Landwirtschaftliche Betriebslehre\\Projekt_P_Bawü\\GAMS\\Crop_Rota_Felix\\Rotationen_Felix.csv", sep=";", stringsAsFactors = T)
-str(Rotations_felix)
+## 01.02.23 reading in own crop rotation file
+Rotations_tristan <-read.csv("C:\\Users\\User\\OneDrive - bwedu\\Dokumente\\Landwirtschaftliche Betriebslehre\\Projekt_P_Bawü\\P_BaWue\\Crop_Rota_Output\\Crop_rotations_Tristan.csv", sep=";", stringsAsFactors = T)
+Rotations_tristan %>% distinct(NUTS_2) %>% count()
+str(Rotations_tristan)
 
 # How many unique Combos are there
-Rotations_felix %>% distinct(Kombo) %>% count()
-Cr_bawue_distinct <- Rotations_felix %>% distinct(Kombo, Gewicht, Glied1, Glied2, Glied3, Glied4, Glied5) %>% select(Kombo:Gewicht)
+Rotations_tristan %>% distinct(Kombo) %>% count()
+Cr_bawue_distinct <- Rotations_tristan %>% distinct(Kombo, Gewicht, Glied1, Glied2, Glied3, Glied4, Glied5) %>% select(Kombo:Gewicht)
 glimpse(Cr_bawue_distinct)
 
-## There are 1193 distinct crop rotations in BAWUE
+## There are 169 distinct crop rotations in BAWUE at the NUTS_2 level
 
 
 ## looking at the variables
@@ -143,15 +135,16 @@ levels(Cr_bawue_distinct$Glied1)
 levels(Cr_bawue_distinct$Glied2)
 levels(Cr_bawue_distinct$Glied3)
 levels(Cr_bawue_distinct$Glied4)
+
+# in der fünfgliederigen Fruchtfolge kommt kein ZR vor
 levels(Cr_bawue_distinct$Glied5)
 summary(Cr_bawue_distinct$Gewicht)
 
-## his file does not include "Rog" and "KL" - will be problem in GAMS sets
 
-## Excluding crop rotations with only one crop
-Cr_bawue_distinct <- Cr_bawue_distinct %>% filter(!Gewicht=="1")
+# ## Excluding crop rotations with only one crop
+# Cr_bawue_distinct <- Cr_bawue_distinct %>% filter(!Gewicht=="1")
 
-### After exclusion: 1185 crop rotation options in BaWUE
+### After exclusion: 169 crop rotation options in BaWUE
 
 
 ## Creating a table containing the crop rotations
@@ -171,38 +164,43 @@ pivotlonger_test <- rotation_matrix %>% pivot_longer(c(Glied1, Glied2, Glied3, G
           pivot_wider(names_from = value, values_from = Gewicht,  values_fill = 0) %>%
           mutate(Gr_Glied1=Gr, KG_Glied1=KG, KM_Glied1=KM, WG_Glied1=WG, 
                  SM_Glied1=SM, WW_Glied1=WW, WR_Glied1=WR, Win_Glied1=Win,
-                 SG_Glied1=SG, Ha_Glied1=Ha, ZR_Glied1=ZR, Ka_Glied1=Ka) %>%
-          select(-c("Gr":"Ka")) %>% select(-name)
+                 SG_Glied1=SG, Ha_Glied1=Ha, ZR_Glied1=ZR, Ka_Glied1=Ka,
+                 Rog_Glied1=Rog, KL_Glied1=KL) %>%
+          select(-c("KM":"KG")) %>% select(-name)
 
 
   Glied2<- pivotlonger_test %>%  filter(name=="Glied2") %>% 
            pivot_wider(names_from = value, values_from = Gewicht, values_fill = 0) %>%
            mutate(Gr_Glied2=Gr, KG_Glied2=KG, KM_Glied2=KM, WG_Glied2=WG, 
                  SM_Glied2=SM, WW_Glied2=WW, WR_Glied2=WR, Win_Glied2=Win,
-                 SG_Glied2=SG, Ha_Glied2=Ha, ZR_Glied2=ZR, Ka_Glied2=Ka) %>%
-           select(-c("KM":"ZR")) %>% select(-name)
+                 SG_Glied2=SG, Ha_Glied2=Ha, ZR_Glied2=ZR, Ka_Glied2=Ka,
+                 Rog_Glied2=Rog, KL_Glied2=KL) %>%
+           select(-c("-":"Gr")) %>% select(-name)
 
   Glied3<- pivotlonger_test %>%  filter(name=="Glied3") %>% 
            pivot_wider(names_from = value, values_from = Gewicht, values_fill = 0) %>%
            mutate(Gr_Glied3=Gr, KG_Glied3=KG, KM_Glied3=KM, WG_Glied3=WG, 
                  SM_Glied3=SM, WW_Glied3=WW, WR_Glied3=WR, Win_Glied3=Win,
-                 SG_Glied3=SG, Ha_Glied3=Ha, ZR_Glied3=ZR, Ka_Glied3=Ka) %>%
-           select(-c("Gr":"WR")) %>% select(-name)
+                 SG_Glied3=SG, Ha_Glied3=Ha, ZR_Glied3=ZR, Ka_Glied3=Ka,
+                 Rog_Glied3=Rog, KL_Glied3=KL) %>%
+           select(-c("-":"KG")) %>% select(-name)
 
   Glied4<- pivotlonger_test %>%  filter(name=="Glied4") %>% 
            pivot_wider(names_from = value, values_from = Gewicht, values_fill = 0) %>%
            mutate(Gr_Glied4=Gr, KG_Glied4=KG, KM_Glied4=KM, WG_Glied4=WG, 
                  SM_Glied4=SM, WW_Glied4=WW, WR_Glied4=WR, Win_Glied4=Win,
-                 SG_Glied4=SG, Ha_Glied4=Ha, ZR_Glied4=ZR, Ka_Glied4=Ka) %>%
+                 SG_Glied4=SG, Ha_Glied4=Ha, ZR_Glied4=ZR, Ka_Glied4=Ka,
+                 Rog_Glied4=Rog, KL_Glied4=KL) %>%
            select(-c("Gr":"-")) %>% select(-name)
 
 
   Glied5<- pivotlonger_test %>%  filter(name=="Glied5") %>% 
-           pivot_wider(names_from = value, values_from = Gewicht, values_fill = 0) %>%
+           pivot_wider(names_from = value, values_from = Gewicht, values_fill = 0) %>% mutate(ZR=0)%>%
            mutate(Gr_Glied5=Gr, KG_Glied5=KG, KM_Glied5=KM, WG_Glied5=WG, 
                  SM_Glied5=SM, WW_Glied5=WW, WR_Glied5=WR, Win_Glied5=Win,
-                 SG_Glied5=SG, Ha_Glied5=Ha, ZR_Glied5=ZR, Ka_Glied5=Ka) %>%
-           select(-c("Gr":"-")) %>% select(-name)
+                 SG_Glied5=SG, Ha_Glied5=Ha, ZR_Glied5=ZR, Ka_Glied5=Ka,
+                 Rog_Glied5=Rog, KL_Glied5=KL) %>%
+           select(-c("-":"ZR")) %>% select(-name)
 
 
 rotation_matrix_full <- Glied1 %>% left_join(Glied2, by="CRid") %>% left_join(Glied3, by="CRid") %>%
@@ -212,8 +210,8 @@ rotation_matrix_full <- rotation_matrix_full %>% select(-c(Kombo.y,Kombo.y.y,Kom
                          
 rm(glied1, Glied1, Glied2, Glied3, Glied4, Glied5)
 rm(glied2, glied3, glied4 ,glied5)
-rm(Crop_share_BW_P)
-rm(Crop_share_BW_test)
+#rm(Crop_share_BW_P)
+
 rm(pivotlonger_test)
 
 ##################################################################################################################################################
@@ -228,13 +226,13 @@ CR_Glieder <- CR_Glieder %>% rename(Cr_Glieder=value)
 
 ## Verknüpfung mit crops
 crops <- Cr_bawue_distinct %>% distinct(Glied1)
-crop_CR_matrix <- rotation_matrix_full %>% slice(1:12)
+crop_CR_matrix <- rotation_matrix_full %>% slice(1:14)
 crop_CR_matrix <- cbind(crops, crop_CR_matrix)
 crop_CR_matrix <- crop_CR_matrix %>% select(-CRid)
 
 
 ## Verknüpfung mit crops
-values_list = c("Gr", "KG", "KM", "WG", "SM", "WW", "WR", "Win", "SG", "Ha", "ZR", "Ka")
+values_list = c("Gr", "KG", "KM", "WG", "SM", "WW", "WR", "Win", "SG", "Ha", "ZR", "Ka","KL", "Rog")
 for (value in values_list) {
   column_name1 = paste0(value, "_Glied1")
   column_name2 = paste0(value, "_Glied2")
@@ -252,13 +250,13 @@ for (value in values_list) {
 
 ### Verknuepfe Info zwischen CR_id=row_id und Kommunen_information
 head(rotation_matrix)
-head(Rotations_felix)
+
 
 
 ### Achtung ich hatte bei rotations_felix filter angelegt... und zwar für eingliedrige Fruchtfolgen die wurden raussortiert
 ### simply switch left_join around
 #kommune_CRid <- Rotations_felix %>% left_join(rotation_matrix, by="Kombo")
-kommune_CRid <- rotation_matrix %>% left_join(Rotations_felix, by="Kombo")
+kommune_CRid <- rotation_matrix %>% left_join(Rotations_tristan, by="Kombo")
 kommune_CRid <- kommune_CRid %>% select(CRid, LAU_ID)
 kommune_CRid
 
@@ -271,22 +269,24 @@ kommune_CRid
 # writing out files related to crop rotation
 
 # Crop_glieder_set
-write_xlsx(x=CR_Glieder, path = "C:/Users/User/OneDrive - bwedu/Dokumente/Landwirtschaftliche Betriebslehre/Projekt_P_Bawü/P_BaWue/CR_Glieder.xlsx", col_names = FALSE)
+write_xlsx(x=CR_Glieder, path = "C:/Users/User/OneDrive - bwedu/Dokumente/Landwirtschaftliche Betriebslehre/Projekt_P_Bawü/P_BaWue/Output_GAMS_P_Prep/CR_Glieder.xlsx", col_names = FALSE)
 
 #crop_CR_matrix, connect crop rotations with crop
-write_xlsx(x=crop_CR_matrix_full, path = "C:/Users/User/OneDrive - bwedu/Dokumente/Landwirtschaftliche Betriebslehre/Projekt_P_Bawü/P_BaWue/crop_CR_matrix.xlsx", col_names = FALSE)
+write_xlsx(x=crop_CR_matrix, path = "C:/Users/User/OneDrive - bwedu/Dokumente/Landwirtschaftliche Betriebslehre/Projekt_P_Bawü/P_BaWue/Output_GAMS_P_Prep/crop_CR_matrix.xlsx", col_names = FALSE)
 
 
 #### kommune_CR_ID, welche croprot kommt in welcher kommune vor?
-write_xlsx(x=kommune_CRid, path = "C:/Users/User/OneDrive - bwedu/Dokumente/Landwirtschaftliche Betriebslehre/Projekt_P_Bawü/P_BaWue/kommune_CRid.xlsx", col_names = TRUE)
+write_xlsx(x=kommune_CRid, path = "C:/Users/User/OneDrive - bwedu/Dokumente/Landwirtschaftliche Betriebslehre/Projekt_P_Bawü/P_BaWue/Output_GAMS_P_Prep/kommune_CRid.xlsx", col_names = TRUE)
 
+## rotation_matrix_full
+write_xlsx(x=rotation_matrix_full, path = "C:/Users/User/OneDrive - bwedu/Dokumente/Landwirtschaftliche Betriebslehre/Projekt_P_Bawü/P_BaWue/Output_GAMS_P_Prep/rotation_matrix_full.xlsx", col_names = TRUE)
 
 
 #######################################################################################################################################################
 ###################################################################################################################################################
 ##### Harmonizing both files
 
-# crop rota files felix
+# crop rota files tristan
 CR_Glieder                                      #|should be ready |contains rotation glied
 crop_CR_matrix                                  #|should be ready |contains crop/rotation_glied
 rotation_matrix_full
